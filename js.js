e3c0e53c8945f1ce6e9f9ff8b4fc63a5a89b4215
luis15herr <herrera05luis@gmail.com>
2023-10-28 22:47:22 -0400

    Animation and email validation

--- js.js
+++ js.js
@@ -3,10 +3,35 @@
 const hideFormButton = document.querySelector(".hide-form-button");
 const newsletterForm = document.querySelector(".newsletter__form");
 const successForm = document.querySelector(".form__success");
+const successBtn = document.querySelector(".form__btn--success");
+const formPosition = newsletterForm.getBoundingClientRect().top;
+const emailInput = document.querySelector(".form__email");
+const form = document.querySelector("form");
 
 hideFormButton.addEventListener("click", () => {
   event.preventDefault();
-  newsletterForm.classList.add("hide");
-  newsletterForm.classList.add("newsletter__form--success");
-  successForm.classList.remove("displayn");
+
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+    form.classList.remove("error");
+    newsletterForm.classList.add("newsletter__form--success");
+    successForm.classList.add("form__success--shown");
+    return true;
+  }
+  emailInput.classList.add("form__email--shaking");
+  form.classList.add("error");
+  return false;
+});
+
+emailInput.addEventListener("animationend", function (e) {
+  console.log(e);
+  if (e.animationName === "horizontal-shaking") {
+    emailInput.classList.remove("form__email--shaking");
+  }
+});
+
+successBtn.addEventListener("click", () => {
+  event.preventDefault();
+  successForm.classList.remove("form__success--shown");
+  newsletterForm.classList.remove("newsletter__form--success");
+  emailInput.value = "";
 });
